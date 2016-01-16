6dcf6b736f723fee645b92b036a11c1c7e5b6a7d
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2016-01-16 15:52:36 -0600

    Move the AccelSensor class, added some tests and made AS support juint tests

---
+++ app/src/main/java/com/example/karenlee/app/sensoranalysis/AccelSensorSnapshot.java
@@ -0,0 +1,164 @@
+package com.example.karenlee.app.sensoranalysis;
+
+import android.util.Log;
+
+import java.util.Arrays;
+
+/**
+ * A class that holds a specific snapshot of accelerometer (or any other 3D sensor data).
+ * Used this to store sensor info and deliver it to the service/thread that finds the BPM.
+ */
+public class AccelSensorSnapshot {
+
+    private static final String TAG = "ACCEL_SNAPSHOT_OBJECT";
+
+    // TODO(Bryce): consider changing these to ArrayLists for easier management iff too many bugs
+    private double[][] accelDataMatrix;
+    private double[] timeDataVector;
+
+    private int currentSample;
+    private int max;
+
+    public AccelSensorSnapshot(int sampleNum) {
+        max = sampleNum;
+        // seperated in case we want to just use this same object.
+        init();
+    }
+
+    private void init() {
+        accelDataMatrix = new double[3][max];
+        timeDataVector = new double[max];
+        currentSample = 0;
+    }
+
+    /**
+     * Stores the given accelerometer sample in this data object, rejecting it if this object is
+     * already filled.
+     * @param time The system time in milliseconds (System.currentTimeMillis())
+     *             TODO(Bryce): adjust this to the time in location 0 if there is something there
+     *             already
+     * @param x The accelerometer measurement in the x direction (horizontal axis of phone,
+     *          positive right side)
+     * @param y The accelerometer measurement in the y direction (vertical axis, positive up)
+     * @param z The accelerometer measurement in the z direction (out of screen of the phone,
+     *          positive out)
+     */
+    public void addSample(float time, float x, float y, float z) {
+        addSample((double) time, (double) x, (double) y, (double) z);
+    }
+
+    /**
+     * Stores the given accelerometer sample in this data object, rejecting it if this object is
+     * already filled.
+     * @param time The system time in milliseconds (System.currentTimeMillis())
+     *             TODO(Bryce): adjust this to the time in location 0 if there is something there
+     *             already
+     * @param x The accelerometer measurement in the x direction (horizontal axis of phone,
+     *          positive right side)
+     * @param y The accelerometer measurement in the y direction (vertical axis, positive up)
+     * @param z The accelerometer measurement in the z direction (out of screen of the phone,
+     *          positive out)
+     */
+    public void addSample(double time, double x, double y, double z) {
+        if (currentSample < max) {
+            // TODO(Bryce): add noise filtering, delta,  and magnitude measurements here.
+            timeDataVector[currentSample] = time;
+            accelDataMatrix[0][currentSample] = x;
+            accelDataMatrix[1][currentSample] = y;
+            accelDataMatrix[2][currentSample] = z;
+            currentSample += 1;
+        } else {
+            Log.d(TAG, "Ran out of space! " + max + " is the max amount.");
+        }
+    }
+
+    public double[] getTimeVec() {
+        return timeDataVector;
+    }
+
+    public double[][] getAccelMatrix() {
+        return accelDataMatrix;
+    }
+
+    public int getNumSamples() {
+        return max;
+    }
+
+    /**
+     * Returns the BPM from the current snapshot it has. Won't let you get the BPM until the
+     * snapshot is filled (will return -1 instead).
+     */
+    public double findBPM() {
+        if (currentSample >= max) {
+            double[] t = timeDataVector;
+            double[][] a = accelDataMatrix;
+            double[] x = a[0];
+            double[] y = a[1];
+            double[] z = a[2];
+            Log.d(TAG, Arrays.toString(x));
+            Log.d(TAG, Arrays.toString(y));
+            Log.d(TAG, Arrays.toString(z));
+            double[] a_mag = new double[t.length];
+            // Loop to find the overall acceleration magnitude of each sample
+            for (int i = 0; i < t.length; i++) {
+                a_mag[i] = Math.sqrt(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]);
+            }
+            Log.d(TAG, Arrays.toString(a_mag));
+            double a_mag_mean = mean(a_mag);
+
+            Log.d(TAG, "" + a_mag_mean);
+
+            // normalize by the mean magnitude (gravity + overall running acceleration)
+            for (int i = 0; i < t.length; i++) {
+                a_mag[i] -= a_mag_mean;
+            }
+            Log.d(TAG, Arrays.toString(a_mag));
+
+            PeakDetector pd = new PeakDetector(a_mag);
+            int[] peakLocations = pd.process(15, standardDeviation(a_mag) * (2.0 / 3.0));
+
+            // correlate the locations of the peaks to times.
+            double[] peakTimes = new double[peakLocations.length];
+            for (int i = 0; i < peakTimes.length; i++) {
+                peakTimes[i] = t[peakLocations[i]];
+            }
+
+            // difference vectors
+            double[] peakDiffs = new double[peakTimes.length - 1];
+            double diffSum = 0.0;
+            for (int i = 0; i < peakTimes.length - 1; i++) {
+                peakDiffs[i] = peakTimes[i + 1] - peakTimes[i];
+                diffSum += peakDiffs[i];
+            }
+
+            // average distance between maxima
+            double avgPeakDistance = diffSum / peakDiffs.length;
+
+            // extrapolate BPM from the average distance
+            return 60.0 / avgPeakDistance;
+        } else {
+            // not enough samples!
+            Log.e(TAG, "Not enough samples to find the BPM. Current: " + (currentSample - 1) +
+                    ", necessary: " + max);
+            return -1;
+        }
+    }
+
+    private static double standardDeviation(double[] x) {
+        double temp = 0;
+        double mean = mean(x);
+        for(double a : x)
+            temp += (mean-a)*(mean-a);
+        return temp/x.length;
+    }
+
+    private static double mean(double[] x) {
+        double sum = 0.0;
+        // loop to find the magnitude total sum, and therefore the mean
+        for(double a : x){
+            sum += a;
+        }
+        return sum/x.length;
+    }
+
+}
---
+++ app/src/test/java/com/example/karenlee/app/AccelSensorSnapshotTest.java
@@ -0,0 +1,73 @@
+package com.example.karenlee.app;
+
+import static org.junit.Assert.*;
+
+import com.example.karenlee.app.sensoranalysis.AccelSensorSnapshot;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for the AccelSensorSnapshot object.
+ */
+public class AccelSensorSnapshotTest {
+
+    private static final int NUM_SAMPLES = 10;
+
+    private AccelSensorSnapshot ass = new AccelSensorSnapshot(NUM_SAMPLES);
+
+    @Test
+    public void testLoadDoubles() {
+        ass.addSample(0.0, 1, 1, 1);
+        for (double i = 0.1; i < 2; i+=.1) {
+            ass.addSample(i, 0, 0, 0);
+        }
+        assertEquals(ass.getAccelMatrix()[0][0], 1, 0.0);
+        // for x, y, z
+        assertEquals(ass.getAccelMatrix().length, 3);
+        assertEquals(ass.getAccelMatrix()[0].length, NUM_SAMPLES);
+    }
+
+    @Test
+    public void testLoadFloats() {
+        ass.addSample((float) .1, (float) 1, (float) 1, (float) 1);
+        for (float i = (float) 0.1; i < 2; i+=.1) {
+            ass.addSample(i, 0, 0, 0);
+        }
+        assertEquals(ass.getAccelMatrix()[0][0], (float) 1, 0.0);
+        // for x, y, z
+        assertEquals(ass.getAccelMatrix().length, 3);
+        assertEquals(ass.getAccelMatrix()[0].length, NUM_SAMPLES);
+    }
+
+    @Test
+    public void testLoadElementsFew() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    @Test
+    public void testOverflowResilient() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    @Test
+    public void testInitToZeros() {
+        assertEquals(ass.getTimeVec()[0], 0.0, 0.0);
+        assertEquals(ass.getTimeVec()[9], 0.0, 0.0);
+    }
+
+    @Test
+    public void testCrashOnReversingInputOrder() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    @Test
+    public void testFindBpmSimple() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    @Test
+    public void testFindBpmAdvanced() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+}
