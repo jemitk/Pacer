672b69a616424a92c38a6468cf3f3f02cd2371d2
jemitk <jemitk15@gmail.com>
2016-01-16 12:09:32 -0600

    logo2

--- app/src/main/java/com/example/karenlee/app/SplashActivity.java
+++ app/src/main/java/com/example/karenlee/app/SplashActivity.java
@@ -1,7 +1,17 @@
 package com.example.karenlee.app;
 
-/**
- * Created by Karen Lee on 1/16/2016.
- */
-public class SplashActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
