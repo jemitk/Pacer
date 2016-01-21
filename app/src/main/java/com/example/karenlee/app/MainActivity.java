298101c1a8cea1ed2f16a99a01c3f121735aaf75
jemitk <jemitk15@gmail.com>
2016-01-21 13:13:06 -0600

    Main activity page changed to adapt to the screen rotation

--- app/src/main/java/com/example/karenlee/app/MainActivity.java
+++ app/src/main/java/com/example/karenlee/app/MainActivity.java
@@ -16,18 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
     static final String TAG = "MAIN_ACTIVITY";
 
+    // note: on create gets called when the screen rotates
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.buttonstart).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 startRun();
             }
         });
-        findViewById(R.id.buttonworkouts).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.workoutsButton).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
